2aefaf4c6e45e8d471ecb8398b731b9eff3afd6a
Pabitra Barua <pabitrabesus@gmail.com>
2025-02-25 17:14:54 +0530

    fixing issues

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { KeywordPage } from "@/components/templates/KeywordPage/KeywordPage";
+import { Routes } from "@/data/routes";
 
 type Props = {
     params: { id: string };
@@ -6,7 +7,7 @@ type Props = {
 
 const Keyword = ({ params }: Props) => {
   const id = params.id;
-  return <KeywordPage currentPage="/articles" id={id} />;
+  return <KeywordPage currentPage={Routes.articles} id={id} />;
 };
 
 export default Keyword;
