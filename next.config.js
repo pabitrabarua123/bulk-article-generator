b2505c35156b6cfb873fd9d5191241129faa2229
Pabitra Barua <pabitrabesus@gmail.com>
2025-04-28 17:59:28 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    serverActions: true,
+  },
   async rewrites() {
     return [
       {
-        source: "/pirsch-extended.js",
-        destination: "https://api.pirsch.io/pirsch-extended.js",
+        source: '/api/:path*',
+        destination: '/api/:path*',
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ]
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
--- src/app/api/cron/job1/route.ts
+++ src/app/api/cron/job1/route.ts
@@ -1,6 +1,9 @@
 import { prismaClient } from "@/prisma/db";
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export async function GET() {
 
   console.log("🕑 Vercel cron job ran every 5 minutes!");
